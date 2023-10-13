581ce525aa9024aa3fc12208807383c70398f2bf
bluesky <chenchongbiao@deepin.org>
2023-10-13 11:49:15 +0800

    chore: update repo
    update version to 0.8.2-1
    
    Issue: https://github.com/linuxdeepin/developer-center/issues/5731
    Log: update version

--- Test/Tasty/SmallCheck.hs
+++ Test/Tasty/SmallCheck.hs
@@ -11,13 +11,13 @@ module Test.Tasty.SmallCheck
 import Test.Tasty.Providers
 import Test.Tasty.Options
 import qualified Test.SmallCheck as SC
-import qualified Test.SmallCheck.Drivers as SC
 import Test.SmallCheck hiding (smallCheck) -- for re-export
-import Test.SmallCheck.Drivers
+import Test.SmallCheck.Drivers as SC
 import Control.Exception
+import Control.Monad (when)
 import Data.Typeable
-import Data.Proxy
 import Data.IORef
+import Options.Applicative (metavar)
 import Text.Printf
 
 -- | Create a 'Test' for a SmallCheck 'SC.Testable' property
@@ -33,13 +33,30 @@ instance IsOption SmallCheckDepth where
   parseValue = fmap SmallCheckDepth . safeRead
   optionName = return "smallcheck-depth"
   optionHelp = return "Depth to use for smallcheck tests"
+  optionCLParser = mkOptionCLParser $ metavar "NUMBER"
+
+-- | The maximum number of test cases to generate. Can be used as an
+-- alternative to setting 'SmallCheckDepth'.
+newtype SmallCheckMaxCount = SmallCheckMaxCount Int
+  deriving (Num, Ord, Eq, Real, Enum, Integral, Typeable)
+
+instance IsOption SmallCheckMaxCount where
+  defaultValue = SmallCheckMaxCount maxBound -- disable by default
+  parseValue = fmap SmallCheckMaxCount . safeRead
+  optionName = return "smallcheck-max-count"
+  optionHelp = return "Maximum smallcheck test count"
+  optionCLParser = mkOptionCLParser $ metavar "NUMBER"
 
 instance IsTest (SC.Property IO) where
-  testOptions = return [Option (Proxy :: Proxy SmallCheckDepth)]
+  testOptions = return
+    [ Option (Proxy :: Proxy SmallCheckDepth)
+    , Option (Proxy :: Proxy SmallCheckMaxCount)
+    ]
 
   run opts prop yieldProgress = do
     let
       SmallCheckDepth depth = lookupOption opts
+      SmallCheckMaxCount maxCount = lookupOption opts
 
     counter <- newIORef (0 :: Int, 0 :: Int)
 
@@ -52,6 +69,8 @@ instance IsTest (SC.Property IO) where
               BadTest -> ((,) $! total + 1) $! bad + 1
 
         count <- myAtomicModifyIORef' counter (\c -> let c' = inc c in (c', fst c'))
+
+        when (count >= maxCount) $ throwIO Finish
 
         -- submit progress data to tasty
         yieldProgress $ Progress
@@ -72,9 +91,17 @@ instance IsTest (SC.Property IO) where
 
     return $
       case scResult of
-        Left e         -> testFailed $ show (e :: SomeException)
+        Left e
+          | Just Finish <- fromException e
+                       -> testPassed desc
+          | otherwise  -> testFailed $ show e
         Right Nothing  -> testPassed desc
         Right (Just f) -> testFailed $ ppFailure f
+
+data Finish = Finish
+  deriving (Eq, Show)
+
+instance Exception Finish
 
 -- Copied from base to stay compatible with GHC 7.4.
 myAtomicModifyIORef' :: IORef a -> (a -> (a,b)) -> IO b
